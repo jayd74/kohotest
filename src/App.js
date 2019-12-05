586eab838fc7a7b9de32cf58df27a30b6fe8f8fa
Jason Du <jayd74@gmail.com>
2019-12-05 13:54:09 -0500

    Initial commit from Create React App

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Transactions from './components/Transactions'
+import './App.css';
+
+function App() {
+  return (
+    <Transactions />
+  )
+}
+
+export default App;
